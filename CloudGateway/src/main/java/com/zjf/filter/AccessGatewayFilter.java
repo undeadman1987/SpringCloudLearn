ae76c2542b43881ff0f1a17305f52204e6ec79b3
zjf <junfeng1987@163.com>
2019-05-28 17:53:03 +0800

    网关增加global filter

---
+++ CloudGateway/src/main/java/com/zjf/filter/AccessGatewayFilter.java
@@ -0,0 +1,46 @@
+package com.zjf.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * 请求url权限校验
+ */
+@Component
+public class AccessGatewayFilter implements GlobalFilter {
+
+    private final static String X_CLIENT_TOKEN_USER = "x-client-token-user";
+    private final static String X_CLIENT_TOKEN = "x-client-token";
+
+    /**
+     * 1.首先网关检查token是否有效，无效直接返回401，不调用签权服务
+     * 2.调用签权服务器看是否对该请求有权限，有权限进入下一个filter，没有权限返回401
+     *
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("this is GlobalFilter.");
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 网关拒绝，返回401
+     *
+     * @param
+     */
+    private Mono<Void> unauthorized(ServerWebExchange serverWebExchange) {
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        DataBuffer buffer = serverWebExchange.getResponse()
+                .bufferFactory().wrap(HttpStatus.UNAUTHORIZED.getReasonPhrase().getBytes());
+        return serverWebExchange.getResponse().writeWith(Flux.just(buffer));
+    }
+}
